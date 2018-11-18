3c539c2e3034010ece62b660c9ca078bccdd89e4
faristoriqu <44657860+faristoriqu@users.noreply.github.com>
2018-11-18 21:56:04 +0700

    register
    sudah bisa js nya, dan add nya sudah siap pakai sekaligus select dengan icon edit dan delete yang kita sepakati

--- include/header.php
+++ include/header.php
@@ -3,9 +3,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>PDW</b></span>
+      <span class="logo-mini"><b>PPS</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>SD </b>Raport</span>
+      <span class="logo-lg"><b>Prima</b>PS</span>
     </a>
 
     <!-- Header Navbar: style can be found in header.less -->
