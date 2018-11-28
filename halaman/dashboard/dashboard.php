b79d85b644a8a52e394308384ac6e2fa5a809197
Syavina Octavia <syavinaoctaviap@gmail.com>
2018-11-28 17:30:41 +0700

    dash

--- halaman/dashboard/dashboard.php
+++ halaman/dashboard/dashboard.php
@@ -1,5 +1,18 @@
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    
+    <section class="content-header">
+       
+      <h1>
+        <i class="ion ion-ios-speedometer"></i>
+        Dashboard 
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
 
-    <!-- Main content -->
     <section class="content">
       <!-- Small boxes (Stat box) -->
       <div class="data">
@@ -7,9 +20,9 @@
           <!-- small box -->
           <div class="small-box bg-aqua">
             <div class="inner">
-              <h3>150</h3>
+              <h3>10</h3>
 
-              <p>New Orders</p>
+              <p>Data Barang</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
@@ -22,12 +35,12 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
+              <h3>10<sup style="font-size: 20px">%</sup></h3>
 
-              <p>Bounce Rate</p>
+              <p>Data Kategori Barang</p>
             </div>
             <div class="icon">
-              <i class="ion ion-stats-bars"></i>
+              <i class="ion ion-filing"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
@@ -37,12 +50,12 @@
           <!-- small box -->
           <div class="small-box bg-yellow">
             <div class="inner">
-              <h3>44</h3>
+              <h3>10</h3>
 
-              <p>User Registrations</p>
+              <p>Data Satuan Barang</p>
             </div>
             <div class="icon">
-              <i class="ion ion-person-add"></i>
+              <i class="ion ion-ios-browsers"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
@@ -52,12 +65,42 @@
           <!-- small box -->
           <div class="small-box bg-red">
             <div class="inner">
-              <h3>65</h3>
+              <h3>10</h3>
 
-              <p>Unique Visitors</p>
+              <p>Data Supplier</p>
             </div>
             <div class="icon">
-              <i class="ion ion-pie-graph"></i>
+              <i class="ion ion-ios-people"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-purple">
+            <div class="inner">
+              <h3>10</h3>
+
+              <p>Data Barang Masuk</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-android-archive"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-lime">
+            <div class="inner">
+              <h3>10</h3>
+
+              <p>Transaksi</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-ios-cart"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
